acbeec07e2951f056ddb55ac6a2a6f8c7a292811
Olha Lavoryk <olhalavoryk@gmail.com>
2023-11-20 15:13:57 +0100

    Works with hardcoded scale factors

--- dm_scale_factors.py
+++ dm_scale_factors.py
@@ -1,44 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description="Plot the tau ID SF")
+parser.add_argument("--era", type=str, default="2016postVFP", help="2016preVFP, 2016postVFP, 2017, 2018")
+args = parser.parse_args()
 
 # Given data
-momentum = ["DM0", "DM1", "DM10_11"]  # Updated momentum bin labels
-correction_factors = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],   # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
+momentum = ["1-prong", r"1 prong +$\pi^{0}$", r"3 prong (+$\pi^{0}$)"]  # Updated momentum bin labels
+
+if args.era == "2016postVFP":
+
+    # postVFP DM binned data
+    correction_factors = np.array([
+        [0.95, 1.02, 0.99],  # vvvloose
+        [0.97, 1.03, 0.96],  # vvloose
+        [0.95, 1.048, 0.95],  # vloose
+        [0.964, 1.024, 0.933],   # loose
+        [0.953, 1.025, 0.95, ],  # medium
+        [0.923, 1.012, 0.939],  # tight
+        [0.897, 0.996, 0.901],  # vtight
+        [0.852, 0.972, 0.903]   # vvtight
+    ])
+
+    up_uncertainties = np.array([
+        [0.05, 0.04, 0.07],  # vvvloose
+        [0.04, 0.04, 0.05],  # vvloose
+        [0.04, 0.033, 0.04],  # vloose
+        [0.035, 0.029, 0.035],  # loose
+        [0.033, 0.028, 0.032],  # medium
+        [0.032, 0.028, 0.031],  # tight
+        [0.034, 0.028, 0.031],  # vtight
+        [0.033, 0.028, 0.031]   # vvtight
+    ])
+
+    down_uncertainties = np.array([
+        [0.05, 0.04, 0.07],  # vvvloose
+        [0.04, 0.04, 0.05],  # vvloose
+        [0.04, 0.033, 0.04],  # vloose
+        [0.035, 0.029, 0.034],  # loose
+        [0.033, 0.028, 0.032],  # medium
+        [0.032, 0.027, 0.031],  # tight
+        [0.034, 0.028, 0.03],  # vtight
+        [0.033, 0.028, 0.032]   # vvtight
 ])
 
-up_uncertainties = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],  # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
-])
 
-down_uncertainties = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],  # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
-])
+#preVFP DM binned data
+
+if args.era == "2016preVFP":
+
+    correction_factors = np.array([
+        [1.08887, 1.03, 1.05],  # vvvloose
+        [1.05, 1.11, 1.05],  # vvloose
+        [1.04, 1.12, 1.06],  # vloose
+        [1.06, 1.096, 1.006],   # loose
+        [1.03, 1.104, 0.973,],  # medium
+        [0.98, 1.09, 0.953,],  # tight
+        [0.932, 1.065, 0.906],  # vtight
+        [0.868, 1.058, 0.905]   # vvtight
+    ])
+
+    up_uncertainties = np.array([
+        [0.06, 0.07, 0.08],  # vvvloose
+        [0.05, 0.05, 0.05],  # vvloose
+        [0.04, 0.04, 0.04],  # vloose
+        [0.04, 0.033, 0.029],  # loose
+        [0.04, 0.03, 0.029],  # medium
+        [0.04, 0.03, 0.029],  # tight
+        [0.035, 0.029, 0.031],  # vtight
+        [0.035, 0.030, 0.03]   # vvtight
+    ])
+
+    down_uncertainties = np.array([
+    [0.05889, 0.07143, 0.09004],
+    [0.04768, 0.04896, 0.05649],
+    [0.04336, 0.0397 , 0.04016],
+    [0.03697, 0.03301, 0.03068],
+    [0.03647, 0.03119, 0.02904],
+    [0.0365, 0.03046, 0.02956],
+    [0.03442, 0.02967, 0.03208],
+    [0.03568, 0.03061, 0.03128],
+    ])
+
 
 working_points = ['vvvloose', 'vvloose', 'vloose', 'loose', 'medium', 'tight', 'vtight', 'vvtight']
-x_labels = ['DM0', 'DM1', 'DM10_11',]
+x_labels = ["1-prong", r"1 prong +$\pi^{0}$", r"3 prong (+$\pi^{0}$)"]
 
 def get_pt_correction_factors(era, user_out_tag, channel, round_to=4):
 
@@ -94,41 +142,60 @@ def get_pt_correction_factors(era, user_out_tag, channel, round_to=4):
 
 
 
-user_out_tags_2016postVFP = ["vvvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vloose_vs_jet_vvloose_vs_ele_6Nov_v2",
-                              "loose_vs_jet_vvloose_vs_ele_6Nov_v2", "medium_vs_jet_vvloose_vs_ele_6Nov_v2",
-                                "tight_vs_jet_vvloose_vs_ele_6Nov_v2", "vtight_vs_jet_vvloose_vs_ele_6Nov_v2", "vvtight_vs_jet_vvloose_vs_ele_6Nov_v2"]
+# user_out_tags_2016postVFP = ["vvvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vloose_vs_jet_vvloose_vs_ele_6Nov_v2",
+#                               "loose_vs_jet_vvloose_vs_ele_6Nov_v2", "medium_vs_jet_vvloose_vs_ele_6Nov_v2",
+#                                 "tight_vs_jet_vvloose_vs_ele_6Nov_v2", "vtight_vs_jet_vvloose_vs_ele_6Nov_v2", "vvtight_vs_jet_vvloose_vs_ele_6Nov_v2"]
 
 
-correction_factors, up_uncertainties, down_uncertainties = get_pt_correction_factors('2016postVFP', user_out_tags_2016postVFP, 'mt',5)
+# correction_factors, up_uncertainties, down_uncertainties = get_pt_correction_factors('2016preVFP', user_out_tags_2016postVFP, 'mt',5)
 
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Correction factors: \n")
+# print(correction_factors)
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Up uncertainties \n")
+# print(up_uncertainties)
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Down uncertainties \n")
+# print(down_uncertainties)
+
+lumilabel = r"16.8 $fb^{-1}$"+" (2016 preVFP UL) (13 TeV)"
+outname = "2016postVFP_scele_factors_DM"
+
+if args.era == "2016preVFP":
+    lumilabel = r"19.7 $fb^{-1}$"+" (2016 preVFP UL) (13 TeV)"
+    outname = "2016preVFP_scele_factors_DM"
 
 
 # Plotting subplots arranged in a table format (2 columns, 4 rows)
 fig, axs = plt.subplots(4, 2, figsize=(10, 12), sharex='col', sharey='row')
 for i, ax in enumerate(axs.flat):
-    ax.errorbar(momentum, correction_factors[i], yerr=[down_uncertainties[i], up_uncertainties[i]], fmt='o', label=working_points[i]+ ' vsJet')
+    ax.errorbar(momentum, correction_factors[i], yerr=[down_uncertainties[i], up_uncertainties[i]], fmt='o', label=working_points[i]+ ' vsJet', elinewidth =3)
     ax.set_xticks(momentum)
-    ax.set_xticklabels(x_labels, fontsize=10)
+    ax.set_xticklabels(x_labels, fontsize=15)
     ax.grid(True)
-    ax.legend(fontsize=10)
+    ax.legend(fontsize=13)
     if i >= 6:  # Only set x-axis label for bottom subplots
-        ax.set_xlabel(r"$\tau$ $p_{T}$", fontsize=12)
+        ax.set_xlabel(r"$\tau$ $p_{T}$"+" GeV", fontsize=15)
     if i % 2 != 0 and i != 7:  # Remove 'Correction Factors' label for right-side plots and last plot
         ax.set_ylabel('')  # Empty label for right-side plots and last plot
         ax.set_title('')
     else:  # Add 'Correction Factors' label for left-side plots except the last one
-        if i == 0 or i == 1:  # Set titles for the first two plots
-            ax.set_title(r"$\tau$ ID scale factors", fontsize=14)
+        if i == 0:  # Set titles for the first two plots
+            ax.set_title(r"$\tau$ ID scale factors", fontsize=20)
         else:
             ax.set_title('')
     if i == 0 or i == 2 or i == 4 or i == 6:
          ax.set_ylabel('Correction Factors', fontsize=12)
+    if i == 1:
+            ax.set_title(lumilabel, fontsize=20)
+    ax.axhline(y=1, color='red', linestyle='--', linewidth = 5)  # Adding the red line at y=1
         
 
 plt.tight_layout()
 
 # Save as PDF and PNG
-plt.savefig("2016postVFP_scele_factors_DM.pdf")
-plt.savefig("2016postVFP_scele_factors_DM.png", dpi=300)  # High-resolution PNG
+plt.savefig(outname+".pdf")
+plt.savefig(outname+".png", dpi=300)  # High-resolution PNG
 
 plt.show()
--- full_sfs_range_plot_pt.py
+++ full_sfs_range_plot_pt.py
@@ -1,41 +1,103 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Plot the tau ID SF")
+parser.add_argument("--era", type=str, default="2016postVFP", help="2016preVFP, 2016postVFP, 2017, 2018")
+args = parser.parse_args()
 
 # Given data
 momentum = np.linspace(1, 5, 5)
-correction_factors = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],   # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
-])
 
-up_uncertainties = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],  # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
-])
 
-down_uncertainties = np.array([
-    [],  # vvvloose
-    [],  # vvloose
-    [],  # vloose
-    [],  # loose
-    [],  # medium
-    [],  # tight
-    [],  # vtight
-    []   # vvtight
-])
+if args.era == "2016postVFP":
+
+    # postVFP pt binned data
+    correction_factors = np.array([
+        [0.93, 0.94, 0.97, 0.99, 0.94 ],  # vvvloose
+        [1.03, 0.98, 1.014,0.996, 0.978],  # vvloose
+        [1.04, 1.005, 1.019, 1.005, 0.988],  # vloose
+        [1.052, 1.0, 1.004, 1.004, 1.002],   # loose
+        [1.005, 0.995, 0.994, 1.011, 1.00],  # medium
+        [1.024, 0.98, 0.982, 1.003, 0.979],  # tight
+        [1.017, 0.973, 0.973, 0.984, 0.961],  # vtight
+        [0.998, 0.959, 0.945, 0.968, 0.935,]   # vvtight
+    ])
+
+    up_uncertainties = np.array([
+        [0.06, 0.04, 0.03, 0.03, 0.03 ],  # vvvloose
+        [0.05, 0.03, 0.028, 0.027, 0.026],  # vvloose
+        [0.04, 0.03, 0.025, 0.024, 0.025],  # vloose
+        [0.036, 0.027, 0.023, 0.023, 0.023],  # loose
+        [0.033, 0.025, 0.023,0.023, 0.023 ],  # medium
+        [0.03, 0.024, 0.022, 0.023, 0.023 ],  # tight
+        [0.033, 0.028, 0.025, 0.027, 0.026],  # vtight
+        [0.028, 0.025, 0.021,0.023, 0.022 ]   # vvtight
+    ])
+
+    down_uncertainties = np.array([
+        [0.06, 0.04, 0.03, 0.03, 0.03 ],  # vvvloose
+        [0.05, 0.03, 0.027, 0.026, 0.026],  # vvloose
+        [0.04, 0.028, 0.024, 0.024, 0.024],  # vloose
+        [0.035, 0.026, 0.022, 0.023, 0.022],  # loose
+        [0.032, 0.024, 0.022, 0.022, 0.022],  # medium
+        [0.03, 0.024, 0.021, 0.022, 0.022],  # tight
+        [0.032, 0.026, 0.025, 0.025, 0.025],  # vtight
+        [0.029, 0.036, 0.032, 0.022, 0.022]   # vvtight
+    ])
+
+
+if args.era == "2016preVFP":
+
+#preVFP pt binned data
+
+    correction_factors = np.array(
+    [
+        [1.09, 1.02, 1.02, 1.02, 1.01],
+        [1.08, 1.00, 1.035, 1.015, 1.016],
+        [1.08596, 1.01, 1.023, 1.008, 1.05047],
+
+        [1.12177, 1.03911, 1.03061, 1.011, 1.03504], #loose
+        [1.07132, 1.008, 1.009, 1.003, 1.002], #medium
+        [1.06487, 0.99579, 1.015, 1.016, 1.01], # tight
+        [1.034, 0.98149, 1.002, 1.004, 0.98306], # vtight
+        [1.037,   0.97556, 0.99091, 0.98637, 0.96467], # vvtight
+    ]
+    )
+
+    up_uncertainties = np.array(
+        [[0.11, 0.06, 0.04, 0.04, 0.04], # vvvloose
+
+        [0.08,  0.05, 0.035, 0.033, 0.032], # vvloose
+
+        [0.0617,  0.04, 0.031,  0.028, 0.029], #vloose
+
+        [0.05, 0.031, 0.02614, 0.024 ,0.02474],  # loose
+
+        [0.03887, 0.028, 0.025, 0.023 ,0.024], # medium
+        [0.03365, 0.02532, 0.023, 0.022, 0.024], # tight
+        [0.032,  0.024 ,0.023,  0.022, 0.02323], #vtight
+        [0.03132, 0.02324, 0.02375, 0.02286, 0.02359],] #vvtight
+    )
+
+    down_uncertainties = np.array(
+
+    [
+    [0.11178, 0.06428, 0.04608, 0.04119, 0.04085], # vvvloose
+    [0.08286, 0.04922, 0.03761, 0.03436, 0.03319], # vvloose
+    [0.06547, 0.0408 , 0.03149, 0.02966, 0.02863], # vloose
+
+    [0.04729, 0.03171, 0.02716, 0.02498, 0.02574], # loose
+    [0.04157, 0.02954, 0.02551, 0.02354, 0.02432], # medium
+    [0.03486, 0.0267 , 0.02444, 0.02353, 0.02393], # tight
+
+    [0.03445, 0.02533, 0.02448, 0.0234,  0.02414], # vtight
+    [0.03335, 0.02435, 0.02463, 0.02307, 0.0243 ], # vvtight
+    ]
+
+    )
 
 working_points = ['vvvloose', 'vvloose', 'vloose', 'loose', 'medium', 'tight', 'vtight', 'vvtight']
 x_labels = ['20-25', '25-30', '30-35', '35-40', '>40']
@@ -101,42 +163,60 @@ def get_pt_correction_factors(era, user_out_tag, channel, round_to=4):
 
 
 
-user_out_tags_2016postVFP = ["vvvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vvloose_vs_jet_vvloose_vs_ele_6Nov_v2", "vloose_vs_jet_vvloose_vs_ele_6Nov_v2",
-                              "loose_vs_jet_vvloose_vs_ele_6Nov_v2", "medium_vs_jet_vvloose_vs_ele_6Nov_v2",
-                                "tight_vs_jet_vvloose_vs_ele_6Nov_v2", "vtight_vs_jet_vvloose_vs_ele_6Nov_v2", "vvtight_vs_jet_vvloose_vs_ele_6Nov_v2"]
+# user_out_tags_2016postVFP = ["vvvloose_vs_jet_vvloose_vs_ele_6Nov_v1", "vvloose_vs_jet_vvloose_vs_ele_6Nov_v1", "vloose_vs_jet_vvloose_vs_ele_6Nov_v1",
+#                               "loose_vs_jet_vvloose_vs_ele_6Nov_v1", "medium_vs_jet_vvloose_vs_ele_6Nov_v1",
+#                                 "tight_vs_jet_vvloose_vs_ele_6Nov_v1", "vtight_vs_jet_vvloose_vs_ele_6Nov_v1", "vvtight_vs_jet_vvloose_vs_ele_6Nov_v1"]
 
 
-correction_factors, up_uncertainties, down_uncertainties = get_pt_correction_factors('2016postVFP', user_out_tags_2016postVFP, 'mt',5)
+# correction_factors, up_uncertainties, down_uncertainties = get_pt_correction_factors('2016preVFP', user_out_tags_2016postVFP, 'mt',5)
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Correction factors: \n")
+# print(correction_factors)
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Up uncertainties \n")
+# print(up_uncertainties)
+
+# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Down uncertainties \n")
+# print(down_uncertainties)
 
 
+lumilabel = r"16.8 $fb^{-1}$"+" (2016 preVFP UL) (13 TeV)"
+outname = "2016postVFP_scele_factors_pt"
 
+if args.era == "2016preVFP":
+    lumilabel = r"19.7 $fb^{-1}$"+" (2016 preVFP UL) (13 TeV)"
+    outname = "2016preVFP_scele_factors_pt"
 
 # Plotting subplots arranged in a table format (2 columns, 4 rows)
 fig, axs = plt.subplots(4, 2, figsize=(10, 12), sharex='col', sharey='row')
 for i, ax in enumerate(axs.flat):
-    ax.errorbar(momentum, correction_factors[i], yerr=[down_uncertainties[i], up_uncertainties[i]], fmt='o', label=working_points[i]+ ' vsJet')
+    ax.errorbar(momentum, correction_factors[i], yerr=[down_uncertainties[i], up_uncertainties[i]], fmt='o', label=working_points[i]+ ' vsJet', elinewidth =3)
     ax.set_xticks(momentum)
-    ax.set_xticklabels(x_labels, fontsize=10)
+    ax.set_xticklabels(x_labels, fontsize=15)
     ax.grid(True)
-    ax.legend(fontsize=10)
+    ax.legend(fontsize=13)
     if i >= 6:  # Only set x-axis label for bottom subplots
-        ax.set_xlabel(r"$\tau$ $p_{T}$", fontsize=12)
+        ax.set_xlabel(r"$\tau$ $p_{T}$ "+" GeV", fontsize=15)
     if i % 2 != 0 and i != 7:  # Remove 'Correction Factors' label for right-side plots and last plot
         ax.set_ylabel('')  # Empty label for right-side plots and last plot
         ax.set_title('')
     else:  # Add 'Correction Factors' label for left-side plots except the last one
         if i == 0 or i == 1:  # Set titles for the first two plots
-            ax.set_title(r"$\tau$ ID scale factors", fontsize=14)
+            ax.set_title(r"$\tau$ ID scale factors", fontsize=20)
         else:
             ax.set_title('')
     if i == 0 or i == 2 or i == 4 or i == 6:
-         ax.set_ylabel('Correction Factors', fontsize=12)
+         ax.set_ylabel('Correction Factors', fontsize=15)
+    if i == 1:
+            ax.set_title(r"19.5 $fb^{-1}$"+" (2016 preVFP UL) (13 TeV)", fontsize=20)
+    ax.axhline(y=1, color='red', linestyle='--', linewidth = 5)  # Adding the red line at y=1
         
 
 plt.tight_layout()
 
 # Save as PDF and PNG
-plt.savefig("2016postVFP_scele_factors.pdf")
-plt.savefig("2016postVFP_scele_factors.png", dpi=300)  # High-resolution PNG
+plt.savefig(outname+".pdf")
+plt.savefig(outname+".png", dpi=300)  # High-resolution PNG
+
 
 plt.show()
